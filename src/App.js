1768cbb9e58254801f138f391308b44ff23df12c
Mitour <love2082020@gmail.com>
2022-08-30 08:32:53 +0800

    add 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Todolist from "./components/Todolist";
+import NotFound from "./components/NotFound";
 import "./css/all.scss";
 import "./css/pages/form.scss";
 import "./css/pages/todolist.scss";
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/todolist" element={<Todolist />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 }
---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+function NotFound() {
+  return (
+    <>
+      <div class="not_found">
+        <h1>404</h1>
+        <h2>頁面不存在！</h2>
+        <img
+          src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/%E5%85%AC%E7%9B%8A%E9%AB%94%E9%A9%97%E7%87%9F-Todolist/empty%201.png"
+          alt="empty"
+        />
+      </div>
+    </>
+  );
+}
+export default NotFound;
